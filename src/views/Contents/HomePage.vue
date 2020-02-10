c1cdb378801978bfdc2e674e586efe6bd2d5076b
CheWei-CHANG <60037371+CheWei-CHANG@users.noreply.github.com>
2020-02-10 13:48:25 +0800

    Contents added
    added informations on homepage

--- src/views/Contents/HomePage.vue
+++ src/views/Contents/HomePage.vue
@@ -8,7 +8,7 @@
                 <v-row class="mb-8">
                     <v-col cols="8">
                         <h1>About US</h1>
-                        <div>文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案</div>
+                        <div>We’re Robomania from Taiwan. Our team is a group of students who are passionate about robots and spare no effort to pursue our dreams. Year 2019 is our Rookie year in the field of FIRST, and we’ve gained a lot through the season. We are looking forward to improve in 2020 and share what we have to the society.</div>
                     </v-col>
                     <v-col>這裡是圖片的位置喔喔喔喔喔喔</v-col>
                 </v-row>
@@ -16,13 +16,13 @@
                     <v-col>這裡是圖片的位置喔喔喔喔喔喔</v-col>                    
                     <v-col cols="8">
                         <h1>FRC 2019</h1>
-                        <div>文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案</div>
+                        <div>We’ve been participating in the Southern Cross Regional Competition located in NSW of Australia in our rookie year, 2019. We’ve gained a lot through the pass season. Our robot, Charlie’s Comet, had been upgraded and improved through the CTSP Pre-competition and the CTSP off season competition.</div>
                     </v-col>
                 </v-row>
                 <v-row class="mb-8">
                     <v-col cols="8">
-                        <h1>Sponsors</h1>
-                        <div>文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案文案</div>
+                        <h1>Outreach</h1>
+                        <div>Outreach program is one of the most important  task that we do. It is very popular to use technology devices nowadays and it is very easy to reach education resources. We want to develop something defferent according to the different stories we’ve experienced. We offer camps to public and we also demonstrate the spirit and our team while science fair and some festivals.</div>
                     </v-col>
                     <v-col>這裡是圖片的位置喔喔喔喔喔喔</v-col>
                 </v-row>
